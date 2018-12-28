408654b9e1057bc49f6861c83d4dab9b62f345c8
Sam Reaves <sreaves@kabbage.com>
2018-12-28 12:23:52 -0500

    Built first programatic view in Swift

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -12,14 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let firstFrame = CGRect(x: 160, y: 240,
+                                width: 100, height: 150)
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.blue
+        view.addSubview(firstView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
