8a315e4291fa72c2dc9c716ce830716ccb3af5b1
Sam Reaves <sreaves@kabbage.com>
2018-12-28 12:57:43 -0500

    Created actual WorldTrotter UI labels and added constraints

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -10,14 +10,5 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let firstFrame = CGRect(x: 160, y: 240,
-                                width: 100, height: 150)
-        let firstView = UIView(frame: firstFrame)
-        firstView.backgroundColor = UIColor.blue
-        view.addSubview(firstView)
-    }
 }
 
