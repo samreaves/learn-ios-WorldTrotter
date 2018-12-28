572ea0b281b65675ad7e3137a41a20b9afe87a3e
Sam Reaves <sreaves@kabbage.com>
2018-12-28 16:49:06 -0500

    Added in delegate method to prevent user input of letters into temperature field. Already had decimal keyboard, but wanted to complete the bronze challenge

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -17,10 +17,13 @@ class ConversionViewController : UIViewController, UITextFieldDelegate {
                    shouldChangeCharactersIn range: NSRange,
                    replacementString string: String) -> Bool {
         
+        
         let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
         let replacementTextHasDecimalSeparator = string.range(of: ".")
+        let replacementTextHasLetters = string.rangeOfCharacter(from: NSCharacterSet.letters)
         
-        if existingTextHasDecimalSeparator != nil, replacementTextHasDecimalSeparator != nil {
+        if (existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil ||
+            replacementTextHasLetters != nil) {
             return false
         } else {
             return true
